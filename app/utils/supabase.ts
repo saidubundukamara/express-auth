eab37e327e3ef1a8c16c8257513182e3bd8846fa
saidubundukamara <bundukamarasaidu@gmail.com>
2024-09-10 11:22:50 +0000

    first commit

---
+++ app/utils/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export function createSupabaseClient() {
+    //@ts-ignore
+    return createClient(process.env.SUPABASE_URL, process.env.SUPERBASE_ANON_KEY);
+}
